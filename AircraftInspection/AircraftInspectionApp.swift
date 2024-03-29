845325434833bd70a57145a3918991ce167cfc51
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-03-29 21:50:07 +0300

    Initial Commit

---
+++ AircraftInspection/AircraftInspectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AircraftInspectionApp.swift
+//  AircraftInspection
+//
+//  Created by Роман on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AircraftInspectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AircraftInspection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AircraftInspection
+//
+//  Created by Роман on 29.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
